b9d2ba04e6e830ef9b6fffc3e75b325aacd189c9
Gary <gauravmohanbansal@gmail.com>
2020-12-16 12:42:37 +0800

    Intial commit

---
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public JSONFileProductService ProductService;
+        public IndexModel(ILogger<IndexModel> logger, JSONFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
